ddcbf9f7758260500640a80993d81f95428d476e
ryota <k014c1106@it-neec.jp>
2023-01-03 00:26:21 +0900

    ViewBainding Sample

--- app/src/main/java/com/example/viewbindingdemoapp/MainActivity.kt
+++ app/src/main/java/com/example/viewbindingdemoapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.viewbindingdemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.viewbindingdemoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.Button.setOnClickListener {
+            binding.TextView.text = "Tapped!!"
+        }
     }
 }
